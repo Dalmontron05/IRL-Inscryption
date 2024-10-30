c22ebceea14214edcb9199c3973f348a827c7264
Dalmontron05 <vexr2dts85338@gmail.com>
2024-10-30 13:19:45 -0700

    started

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 // Imports
 #include <iostream>
 #include <string>
+#include <vector>
 
 // #include "main.h"
 #include "Color-Codes.cpp"
@@ -14,6 +15,25 @@ using namespace std;
 
 
 // Classes
+class Sigils
+{
+public:
+    vector<string> name = {"Airborne", "Amorphous", "Ant Spawner", "Bees Within", "Bellist", "Bifurcated Strike", "Bone King", "Burrower", "Corpse Eater", "Dam Builder", "Fecundity", "Fledling", "Frozen Away", "Guardian", "Hefty", "Hoarder", "Leader", "Loose Tail", "Many Lives", "Mighty Leap", "Omni Strike", "Rabbit Hole", "Repulsive", "Sharp Quills", "Sprinter", "Steel Trap", "Stinky", "Tidal Lock", "Touch of Death", "Trifurcated Strike", "Trinket Bearer", "Unkillable", "Waterborne", "Worthy Sacrifice"};
+
+    vector<string> description = {};
+};
+
+class Card
+{
+public:
+    vector<string> name = {"Stoat"};
+    vector<bool> costTypeIsBlood = {true}; // can either be blood or bones
+    vector<int> costAmount = {1}; // blood range: 1-4 bones: 1-infinity
+    vector<int> attackAmount = {1};
+    vector<int> healthAmount = {3};
+};
+
+class Stoat
 
 
 
--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This is just a program accompanying my IRL version of Inscryption. It's based on
 
 # Differences from Kaycees mod
 Stoat went from 2 health to 3 health
+
+# Tje plan
+- create a class for both the total cards and cards in a players hand
+- class for sigils names, icon (maybe ascii arts), and description
+- fecundity can only create one copy of a card (even if you have unkillable, it won't come back every other card) (sorry leshy and kaycee, had to be done).
