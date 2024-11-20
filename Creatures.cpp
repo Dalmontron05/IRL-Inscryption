a8b4ce05364e7fb82d6072d8f85bfaffbec2f03b
Dalmontron05 <vexr2dts85338@gmail.com>
2024-11-20 03:00:34 -0700

    added a few creatures

--- Creatures.cpp
+++ Creatures.cpp
@@ -14,11 +14,12 @@ class Creature
 protected:
     // Attributes
     string name = "bruh";
+    bool canBeGained; // Is a card that can be gained through card events
     bool costTypeIsBlood; // can either be blood or bones
     int cost; // blood range: 0-4. bones: 1-infinity (free cards are still considered blood type cards for this implementation)
     int attack;
     int health;
-    // Can have the follwing tribes: Squirrel, 
+    // Can have the following tribes: Squirrel, Reptile, Insect, Avian, Canine, Hooved
     vector<string> tribe;
     // Can have the following sigils: {"None", "Airborne", "Amorphous", "Ant Spawner", "Bees Within", "Bellist", "Bifurcated Strike", "Bone King", "Burrower", "Corpse Eater", "Dam Builder", "Fecundity", "Fledling", "Frozen Away", "Guardian", "Hefty", "Hoarder", "Leader", "Loose Tail", "Many Lives", "Mighty Leap", "Omni Strike", "Rabbit Hole", "Repulsive", "Sharp Quills", "Sprinter", "Steel Trap", "Stinky", "Tidal Lock", "Touch of Death", "Trifurcated Strike", "Trinket Bearer", "Unkillable", "Waterborne", "Worthy Sacrifice"};
     vector<string> sigils;
@@ -26,7 +27,7 @@ protected:
 
 public:
     // Constructor
-    Creature(string name, bool costTypeIsBlood, int cost, int attack, int health, vector<string> tribe, vector<string> sigils)
+    Creature(string name, bool canBeGained, bool costTypeIsBlood, int cost, int attack, int health, vector<string> tribe, vector<string> sigils)
         : name(name), costTypeIsBlood(costTypeIsBlood), cost(cost), attack(attack), health(health), tribe(tribe), sigils(sigils) {}
 
 
@@ -43,21 +44,28 @@ class Squirrel : public Creature
 {
 public:
     Squirrel()
-    : Creature("Squirrel", true, 0, 0, 1, {"Squirrel"}, {"None"}) {}
+    : Creature("Squirrel", false, true, 0, 0, 1, {"Squirrel"}, {"None"}) {}
 };
 
 
 // Main Deck
+class Bullfrog : public Creature
+{
+    Bullfrog()
+    : Creature("Bullfrog", true, true, 1, 1, 2, {"Reptile"}, {"Mighty Leap"}) {}
+};
+
+
 class Stoat : public Creature
 {
 public:
     Stoat()
-    : Creature("Stoat", true, 1, 1, 3, {"None"}, {"None"}) {}
+    : Creature("Stoat", true, true, 1, 1, 3, {"None"}, {"None"}) {}
 };
 
-
-// class DireWolf : public Creature
-// {
-//     string name = "Dire Wolf";
-// };
+class Wolf : public Creature
+{
+    Wolf()
+    : Creature("Wolf", true, true, 2, 3, 2, {"Canine"}, {"None"}) {}
+};
 
